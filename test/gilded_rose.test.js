d80c714f27f0544826468789b8b9200462f179f2
Endika Orube <endika.orube@biko2.com>
2024-05-07 09:23:15 +0200

    init: base del proyecto

---
+++ test/gilded_rose.test.js
@@ -0,0 +1,28 @@
+const { updateQuality, createItem } = require("../src/gilded_rose");
+
+const items = [
+  createItem("+5 Dexterity Vest", 10, 20),
+  createItem("Aged Brie", 2, 0),
+  createItem("Elixir of the Mongoose", 5, 7),
+  createItem("Sulfuras, Hand of Ragnaros", 0, 80),
+  createItem("Sulfuras, Hand of Ragnaros", -1, 80),
+  createItem("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  createItem("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  createItem("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+
+  // This Conjured item does not work properly yet
+  createItem("Conjured Mana Cake", 3, 6),
+];
+
+const days = Number(process.argv[2]) || 2;
+
+console.log("OMGHAI!");
+for (let day = 0; day < days + 1; day++) {
+  console.log(`-------- day ${day} --------`);
+  console.log("name, sellIn, quality");
+  items.forEach((item) =>
+    console.log(`${item.name}, ${item.sellIn}, ${item.quality}`)
+  );
+  updateQuality(items);
+  console.log("");
+}
